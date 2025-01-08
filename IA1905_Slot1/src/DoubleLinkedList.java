0063a3025ccf65225aeddaa911bf0f05485580aa
Kim Minh Đức <kminhduc.874@gmail.com>
2025-01-08 10:42:25 +0700

    Slot2

---
+++ IA1905_Slot1/src/DoubleLinkedList.java
@@ -0,0 +1,196 @@
+
+public class DoubleLinkedList {
+    // Node class
+    private static class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private Node head; // Head node of the list
+    private Node tail; // Tail node of the list
+
+    // Constructor
+    public DoubleLinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    // Add a node at the end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Add a node at the beginning
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    // Remove a node by value
+    public void remove(int data) {
+        if (head == null) return;
+
+        Node current = head;
+
+        while (current != null) {
+            if (current.data == data) {
+                if (current == head) {
+                    head = current.next;
+                    if (head != null) head.prev = null;
+                } else if (current == tail) {
+                    tail = current.prev;
+                    if (tail != null) tail.next = null;
+                } else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                return;
+            }
+            current = current.next;
+        }
+    }
+    
+    public void removeAt(int index) {
+        if (head == null) {
+            System.out.println("List is empty.");
+            return;
+        }
+
+        if (index == 0) { // Remove the head node
+            if (head.next == null) { // Only one node in the list
+                head = null;
+                tail = null;
+            } else {
+                head = head.next;
+                head.prev = null;
+            }
+            return;
+        }
+
+        Node current = head;
+        int currentIndex = 0;
+
+        // Traverse to the node at the given index
+        while (current != null && currentIndex < index) {
+            current = current.next;
+            currentIndex++;
+        }
+
+        if (current == null) { // Index out of bounds
+            System.out.println("Index out of bounds.");
+            return;
+        }
+
+        if (current.next == null) { // Remove the tail node
+            tail = current.prev;
+            tail.next = null;
+        } else { // Remove a middle node
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+    }
+    
+    // Insert a node at a specific position
+    public void insert(int data, int position) {
+        Node newNode = new Node(data);
+
+        // If inserting at the beginning
+        if (position == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node current = head;
+        int currentIndex = 0;
+
+        // Traverse the list to find the position
+        while (current != null && currentIndex < position - 1) {
+            current = current.next;
+            currentIndex++;
+        }
+
+        // If position is beyond the last node, insert at the end
+        if (current == null) {
+            addLast(data);
+        } else {
+            // Insert in between nodes
+            newNode.next = current.next;
+            newNode.prev = current;
+            if (current.next != null) {
+                current.next.prev = newNode;
+            }
+            current.next = newNode;
+
+            // Update the tail if inserted at the end
+            if (newNode.next == null) {
+                tail = newNode;
+            }
+        }
+    }
+
+    // Print the list from head to tail
+    public void printForward() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    // Print the list from tail to head
+    public void printBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+    
+     // Search for a value from head to tail
+    public boolean searchForward(int data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                return true; // Found the value
+            }
+            current = current.next;
+        }
+        return false; // Value not found
+    }
+
+    // Search for a value from tail to head
+    public boolean searchBackward(int data) {
+        Node current = tail;
+        while (current != null) {
+            if (current.data == data) {
+                return true; // Found the value
+            }
+            current = current.prev;
+        }
+        return false; // Value not found
+    }
+}
---
+++ IA1905_Slot1/src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DoubleLinkedList dll = new DoubleLinkedList();
+
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.addLast(30);
+        dll.printForward(); // Output: 10 20 30
+
+        dll.addFirst(5);
+        dll.printForward(); // Output: 5 10 20 30
+
+        dll.remove(20);
+        dll.printForward(); // Output: 5 10 30
+
+        dll.printBackward(); // Output: 30 10 5
+        
+        dll.insert(5, 0); // Insert at the beginning
+        dll.printForward(); // Output: 5 10 20 30
+
+        dll.insert(25, 3); // Insert at position 3
+        dll.printForward(); // Output: 5 10 20 25 30
+
+        dll.insert(40, 10); // Insert beyond the last node (at the end)
+        dll.printForward(); // Output: 5 10 20 25 30 40
+    }
+    
+}
